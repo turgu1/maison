7a490b3ccc2b61b4690c7e840377a502185c32c2
turgu1 <turgu666@gmail.com>
2019-01-06 22:56:25 -0500

    Bug chase

--- src/Maison.cpp
+++ src/Maison.cpp
@@ -886,7 +886,7 @@ uint32_t Maison::CRC32(const uint8_t * data, size_t length)
       if (i < 19) debug(F(",")); 
     }
     debugln(F("]"));
-    debugln(F("---- End ----"));
+    debugln(F("---- The End ----"));
   }
 
 #endif
