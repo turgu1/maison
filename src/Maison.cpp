9ec65ee840dbcc9eb014db46b0d71c8580c02930
turgu1 <turgu666@gmail.com>
2019-01-13 10:31:00 -0500

    init_user_mem debugging.

--- src/Maison.cpp
+++ src/Maison.cpp
@@ -838,8 +838,10 @@ bool Maison::init_user_mem()
 
   SHOW("init_user_mem()");
 
-  //byte *ptr = user_mem;
-  //for (int i = 0; i < user_mem_length; i++) *ptr++ = 0;
+  DEBUG(F("User Memory Length: "));
+  DEBUGLN(user_mem_length);
+  byte *ptr = user_mem;
+  // for (int i = 0; i < user_mem_length; i++) *ptr++ = 0;
   //memset(user_mem, 0, user_mem_length);
 
   bool result = true; //write_mem((uint32_t *) &user_mem, user_mem_length, sizeof(mem));
